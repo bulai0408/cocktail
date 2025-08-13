510f1b3deb6ddba5811699506922a4512c94e4a9
Your Name <you@example.com>
2025-08-14 00:29:25 +0800

    Initial Commit

---
+++ CocktailApp/CocktailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAppApp.swift
+//  CocktailApp
+//
+//  Created by 蔡煜恒 on 2025/8/14.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocktailApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CocktailApp
+//
+//  Created by 蔡煜恒 on 2025/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
